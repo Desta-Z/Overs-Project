29ccc47f04a0d19dab4f77ff32d3de65ecdf624c
Desta-Z <desta.zerihun@astu.edu.et>
2020-09-23 11:24:36 +0300

    Add project files.

---
+++ OVERS/Controllers/CustomerRegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OVERS.Models;
+namespace OVERS.Controllers
+{
+    public class CustomerRegistrationController : Controller
+    {
+        private readonly CustomerDB cd; 
+        public CustomerRegistrationController(CustomerDB _cd)
+        {
+            cd = _cd;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Customer c)
+        {
+            cd.Add(c);
+            cd.SaveChanges();
+            return View();
+
+            //VieweBag
+        }
+        
+    }
+}
---
+++ OVERS/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace OVERS.Models
+{
+    public class Customer
+    {
+         [Key]
+        [Display(Name ="Customer ID")]
+        public int cid { get; set; }
+        [Display(Name ="First Name")]
+        public string fname { get; set; }
+        [Display(Name = "Last Name")]
+        public string lname { get; set; }
+        [Display(Name = "Current Address")]
+        public string currentaddress { get; set; }
+        [Display(Name = "Country")]
+        public string country { get; set; }
+
+    }
+}
